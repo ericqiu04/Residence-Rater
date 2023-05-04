9f658da81071348c46ae90ea5218c8582a25e8a4
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-05-03 22:42:14 -0400

    created a python code that scrapes ontariouniversities.ca and gets all the universities in beautiful soup soup and then stores it in a dictionary in JS for later frontend use

---
+++ uni_list.js
@@ -0,0 +1,146 @@
+export const universities = [
+  {
+    id: 1,
+    name: "Algoma University",
+  },
+  {
+    id: 2,
+    name: "Brock University",
+  },
+  {
+    id: 3,
+    name: "Carleton University",
+  },
+  {
+    id: 4,
+    name: "Lakehead University",
+  },
+  {
+    id: 5,
+    name: "Laurentian University",
+  },
+  {
+    id: 6,
+    name: "McMaster University",
+  },
+  {
+    id: 7,
+    name: "OCAD University",
+  },
+  {
+    id: 8,
+    name: "Ontario Tech University",
+  },
+  {
+    id: 9,
+    name: "Queen's University",
+  },
+  {
+    id: 10,
+    name: "Royal Military College of Canada",
+  },
+  {
+    id: 11,
+    name: "Toronto Metropolitan University",
+  },
+  {
+    id: 12,
+    name: "Trent University",
+  },
+  {
+    id: 13,
+    name: "Trent University � Durham Greater Toronto Area",
+  },
+  {
+    id: 14,
+    name: "Universit� de Hearst",
+  },
+  {
+    id: 15,
+    name: "Universit� de l'Ontario fran�ais",
+  },
+  {
+    id: 16,
+    name: "University of Guelph",
+  },
+  {
+    id: 17,
+    name: "University of Guelph-Humber",
+  },
+  {
+    id: 18,
+    name: "University of Ottawa",
+  },
+  {
+    id: 19,
+    name: "University of Ottawa � Saint Paul University",
+  },
+  {
+    id: 20,
+    name: "University of Toronto",
+  },
+  {
+    id: 21,
+    name: "University of Toronto � Mississauga",
+  },
+  {
+    id: 22,
+    name: "University of Toronto � Scarborough",
+  },
+  {
+    id: 23,
+    name: "University of Waterloo",
+  },
+  {
+    id: 24,
+    name: "University of Waterloo � Conrad Grebel University College",
+  },
+  {
+    id: 25,
+    name: "University of Waterloo � Renison University College",
+  },
+  {
+    id: 26,
+    name: "University of Waterloo � St. Jerome's University",
+  },
+  {
+    id: 27,
+    name: "University of Waterloo � United College",
+  },
+  {
+    id: 28,
+    name: "University of Windsor",
+  },
+  {
+    id: 29,
+    name: "Western University",
+  },
+  {
+    id: 30,
+    name: "Western University � Brescia University College",
+  },
+  {
+    id: 31,
+    name: "Western University � Huron University College",
+  },
+  {
+    id: 32,
+    name: "Western University � King's University College",
+  },
+  {
+    id: 33,
+    name: "Wilfrid Laurier University",
+  },
+  {
+    id: 34,
+    name: "Wilfrid Laurier University � Brantford Campus",
+  },
+  {
+    id: 35,
+    name: "York University",
+  },
+  {
+    id: 36,
+    name: "York University � Glendon Campus",
+  },
+];

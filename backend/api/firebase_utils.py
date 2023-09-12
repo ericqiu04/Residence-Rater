4f164ccea5b2938497d715f198ae183c571112e0
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-12 17:01:11 -0400

    initialized client side with next.js

---
+++ backend/api/firebase_utils.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('credentials/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def fetch_user():
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
+
+    user_data = []
+
+    for doc in docs:
+        user_data.append(doc.to_dict())
+
+    return render(request, 'fetch_data.html', {'user_data': user_data})
+        
+    
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.db import models
 from django.http import HttpResponse
 
+#firebase
+from .firebase_utils import fetch_user
+
 def hello_world(request):
     return HttpResponse("Hello, World!")
+
+def test():
+    fetch_user()

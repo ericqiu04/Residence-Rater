ded2159cc6cae75dbf637e1aebb8274792b009ad
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-14 20:25:14 -0400

    linked firebase and backend. Allowed users to store login information into firebase

--- backend/api/firebase_utils.py
+++ backend/api/firebase_utils.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import request
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, db
 
-cred = credentials.Certificate('../credentials/serviceAccountKey.json')
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("../credentials/serviceAccountKey.json")
+firebase_admin.initialize_app(
+    cred, {"databaseURL": "https://ontario-residence-rater.firebaseio.com"}
+)
 
-db = firestore.client()
+data = firestore.client()
+
+
+def save_user(name, email, password):
+    ref = data.collection("users")
+
+    user_id = data.collection("users").document().id
+
+    ref.document(user_id).set({"name": name, "email": email, "password": password})
+
+    return user_id
+
 
 def fetch_user():
-    users_ref = db.collection(u'users')
+    users_ref = data.collection("users")
     docs = users_ref.stream()
 
     user_data = []
@@ -17,6 +30,4 @@ def fetch_user():
     for doc in docs:
         user_data.append(doc.to_dict())
 
-    return render(request, 'fetch_data.html', {'user_data': user_data})
-        
-    
+    return render(request, "fetch_data.html", {"user_data": user_data})
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,22 +3,27 @@ from django.db import models
 from django.http import HttpResponse, JsonResponse
 import json
 
-#firebase
-from .firebase_utils import fetch_user
+
+# firebase
+from .firebase_utils import fetch_user, save_user
+
 
 def hello_world(request):
     return HttpResponse("Hello, World!")
 
+
 def user_login(request):
     data = json.loads(request.body)
-    name = data.get('name')
-    password = data.get('password')
-    email = data.get('email')
-    return JsonResponse({
-        "name": name,
-        "password": password,
-        "email": email
-    })
+    name = data.get("name")
+    password = data.get("password")
+    email = data.get("email")
+
+    user_id = save_user(name, email, password)
+    if user_id:
+        return JsonResponse({"id": user_id})
+
+    else:
+        return JsonResponse({"id": "unknown"})
 
 
 def test():

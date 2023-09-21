2bab5bcca2db5a5a6eb870c1073ca41212beabf5
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-21 14:27:40 -0400

    changed collection from "university" to "universities" which solved minor errors with database input

--- backend/api/rating.py
+++ backend/api/rating.py
@@ -1,13 +1,13 @@
-import firebase_admin
 from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate("../credentials/serviceAccountKey.json")
 db = firestore.client()
-user_ref = db.collection("university")
+user_ref = db.collection("universities")
 
 def create_review(uni_name, res_name, user, message, rating_value):
     uni_ref = user_ref.document(uni_name.lower())
     house_ref = uni_ref.collection('residence').document(res_name.lower())
+    print(res_name)
     rating_ref = house_ref.collection('rating')
     print(user)
     rating = rating_ref.document(user.lower())

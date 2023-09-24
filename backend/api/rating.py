3264a3d20543d8cf8246ecb03d90979d8d51721b
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-23 21:40:20 -0400

    created a function that calculates the average rating of the residences.

--- backend/api/rating.py
+++ backend/api/rating.py
@@ -11,7 +11,7 @@ def create_review(uni_name, res_name, user, message, rating_value):
         "message": message,
         "rating": rating_value
     })
-    
+    average_rating(uni_name, res_name)
     return {'success': 'rating_created'}
 
 def update_review(uni_name, res_name, user, data):
@@ -23,6 +23,7 @@ def update_review(uni_name, res_name, user, data):
         return {'error': 'rating does not exist'}
     update_data = {**current_data, **data}
     rating.update(update_data)
+    average_rating(uni_name, res_name)
     return {'success': 'rating updated'}
 
 def delete_review(uni_name, res_name, user):
@@ -33,11 +34,31 @@ def delete_review(uni_name, res_name, user):
         return {'error': 'rating does not exist'}
     
     rating_ref.document(user.lower()).delete()
-
+    average_rating(uni_name, res_name)
     return {'success': 'rating has been deleted'}
 
+def average_rating(uni_name, res_name):
+    total_rating = 0
+    num_rating = 0
+    rating_ref = get_rating_ref(uni_name, res_name)
+    house_ref = user_ref.document(uni_name.lower()).collection('residence').document(res_name.lower())
+    ratings = rating_ref.stream()
+
+    for r in ratings:
+        data = r.to_dict()
+        rating = data.get('rating')
+        total_rating += rating
+        num_rating += 1
+    
+    if total_rating > 0:
+        average = total_rating / num_rating
+        house_ref.set({
+            'average rating': average
+        })
 
 def get_rating_ref(uni_name, res_name):
     uni_ref = user_ref.document(uni_name.lower())
     house_ref = uni_ref.collection('residence').document(res_name.lower())
     return house_ref.collection('rating')
+
+    

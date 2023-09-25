978fa216914618c9f0c3d276fc521402b8483341
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-24 22:49:12 -0400

    created basic navbar for web, mobile will be added later

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,70 +1,34 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import Link from "next/link";
 
-
-interface State {
-  name: string;
-  password: string;
-  email: string;
-}
-class Home extends React.Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      name: "",
-      password: "",
-      email: "",
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  async handleSubmit(e: any) {
-    e.preventDefault();
-    const { name, password, email } = this.state;
-    axios
-      .post("http://localhost:8000/api/user_login", { name, password, email })
-      .then((response) => {
-        console.log("Form submitted successfully:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error submitting form:", error);
-      });
-  }
-
-  render() { 
+class Home extends React.Component {
+  render() {
     return (
       <>
-        <div className="flex h-screen justify-center items-center">
-          <form
-            className="flex flex-row space-x-3"
-            onSubmit={this.handleSubmit}
-          >
-            <input
-              className="border-black border-2 px-2 rounded-md"
-              placeholder="name"
-              type="text"
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-            <input
-              className="border-black border-2 px-2 rounded-md"
-              placeholder="password"
-              type="text"
-              onChange={(e) => this.setState({ password: e.target.value })}
-            />
-            <input
-              className="border-black border-2 px-2 rounded-md"
-              placeholder="email"
-              type="email"
-              onChange={(e) => this.setState({ email: e.target.value })}
-            />
-            <button
-              className="border-black border-2 px-2 rounded-md"
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
+        <div className="">
+          {/* NAVBAR */}
+          <nav className="p-4">
+            <div className="container mx-auto">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center space-x-4">
+                  <Link href="/">
+                    <h3 className="text-default font-bold text-xl">
+                      Residence Rater
+                    </h3>
+                  </Link>
+                </div>
+                <div className="flex items-center space-x-10"> {/* Changed from hidden md:flex */}
+                  <Link href="/residences">
+                    <h4 className="text-default text-lg">Universities</h4>
+                  </Link>
+                  {/* Turn into an icon later */}
+                  <Link href="/contact">
+                    <h4 className="text-default text-lg">Login / Register</h4>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </nav>
         </div>
       </>
     );

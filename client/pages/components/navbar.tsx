36a9f70dece4ff457419fcaacdc3ff1c2889c829
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-25 21:14:15 -0400

    created animation for search bar

--- client/pages/components/navbar.tsx
+++ client/pages/components/navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "next/link";
 import { MdSearch } from "react-icons/md";
+import { motion } from 'framer-motion';
+
+
 
 interface NavbarState {
   showSearchBar: boolean;
@@ -31,7 +34,7 @@ class Navbar extends React.Component<{}, NavbarState> {
 
   render() {
     return (
-      <nav className="p-4">
+      <nav className="p-5">
         <div className="container mx-auto">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-4">
@@ -43,27 +46,28 @@ class Navbar extends React.Component<{}, NavbarState> {
             </div>
             <div className="flex items-center space-x-10">
               {this.state.showSearchBar && (
-                <form
-                  onSubmit={this.handleSearchSubmit}
-                  className="flex items-center"
-                >
-                  <input
+                <motion.form
+                initial={{ width: 140 }}
+                animate={{ width: this.state.showSearchBar ? 175 : 0 }}
+                transition={{ duration: 0.3 }}
+                onSubmit={this.handleSearchSubmit}
+                className="flex items-center search-bar "
+            >
+                <input
                     type="text"
                     value={this.state.searchQuery}
                     onChange={this.handleSearchChange}
-                    className="border rounded py-1 px-2"
+                    className=" border-b-2 border-solid px-2 w-96 focus:outline-none "
                     placeholder="Search..."
-                  />
-                  <button
-                    type="submit"
-                    className="bg-blue-500 text-white px-3 py-1 rounded"
-                  >
-                    Search
-                  </button>
-                </form>
+                />
+                <MdSearch
+                    className="text-default text-xl cursor-pointer"
+                    onClick={this.handleSearchSubmit}
+                />
+            </motion.form>
               )}
               <button onClick={this.handleSearchClick}>
-                <MdSearch className="text-default text-xl cursor-pointer" />
+                <MdSearch className="text-default text-xl cursor-pointer z-10" />
               </button>
               <Link href="/residences">
                 <h4 className="text-default text-lg">Universities</h4>

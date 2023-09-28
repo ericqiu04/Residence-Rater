6f5c65577e79b4aaa056c2a025210e3510d963a9
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-09-27 20:39:19 -0400

    added SEO tag for landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Head from "next/head";
 class Home extends React.Component {
   state = {
     showSearchBar: false,
-    searchQuery: ""
-  }
+    searchQuery: "",
+  };
 
-  handleClick = {
-
-  }
+  handleClick = {};
 
   render() {
     return (
       <>
+        <Head>
+          <title>Home | Residence Rater</title>
+          <meta
+            name="landing page"
+            content="landing page for Residence Rater. Page will include a navigation bar to login or signup and find universities, as well as a button that navigates to the universities."
+          />
+        </Head>
         <div className="w-full">
           {/* NAVBAR */}
-          <Navbar/>
+          <Navbar />
         </div>
       </>
     );

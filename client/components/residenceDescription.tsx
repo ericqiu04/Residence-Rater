f6d40fdab745a972a702e0b46ed31b8dda604661
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-10-04 22:51:39 -0400

    new file

---
+++ client/components/residenceDescription.tsx
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class ResidenceDescription extends Component {
+
+    render(){
+        return(
+            <>
+                <div>
+                    
+                </div>
+            </>
+        )
+    }
+}
